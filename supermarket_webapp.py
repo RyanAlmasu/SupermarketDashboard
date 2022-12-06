3bbc7b4b7d3eca21bcce8332fa35e99eb44e9664
RyanAlmasu <ryanalimasud3@gmail.com>
2022-12-06 21:55:17 +0700

    Supermarket Dashboard Update 2

--- supermarket_webapp.py
+++ supermarket_webapp.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-
+import openpyxl 
 
 st.set_page_config(page_title= 'Sales Dashboard',
                    page_icon=':bar_chart:', 
